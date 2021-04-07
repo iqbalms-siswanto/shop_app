04888b9fa4f90566274e041ff2312b02019571c2
Iqbal muhamad siswanto <iqbalbesari@gmail.com>
2021-04-07 14:56:36 +0700

    main dart commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
-import 'package:shop_app/screens/home/home_screen.dart';
 import 'package:shop_app/navigation_homescreen.dart';
 
 void main() {
